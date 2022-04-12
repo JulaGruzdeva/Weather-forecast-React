e1930474c7471744be3849377692a8c0da2c14c2
Julia Gruzdeva <yulasha.gruzdeva@gmail.com>
2022-04-12 17:35:37 +0300

    changed placeholder

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -6,7 +6,7 @@ const Input = ({ getWeather }) => {
     const [city, setCity] = useState('');
     return (
         <div className="InputBlock">
-            <input onChange={(e) => setCity((e.target.value))} placeholder='Input city' />
+            <input onChange={(e) => setCity((e.target.value))} placeholder='введите город' />
             <BiSearchAlt onClick={() => { if (city) { getWeather(city) } }} className="BiSearchAlt" />
 
         </div>
